f933d86ed8d957855e48269fcc937c88fb128a54
Jaron <jaronritter01@gmail.com>
2022-04-23 21:21:44 -0500

    refactored the the slime and the projectile code

--- Player.gd
+++ Player.gd
@@ -13,6 +13,8 @@ var projectile = preload("res://ProjectileLight.tscn")
 var facing = 1
 var jumpCount = 0
 var lives = 3
+var player = self
+var can_move = true
 
 func _ready():
 	timer.set_wait_time(.8)
@@ -32,28 +34,29 @@ func _physics_process(_delta):
 	if is_on_floor():
 		jumpCount = 0
 	### run by game 60x/sec
-	if Input.is_action_pressed("move_left"):
-		velocity.x = -SPEED
-		$PlayerAnimations.flip_h = true
-		facing = -1
+	if can_move:
+		if Input.is_action_pressed("move_left"):
+			velocity.x = -SPEED
+			$PlayerAnimations.flip_h = true
+			facing = -1
+			
+		if Input.is_action_pressed("move_right"):
+			velocity.x = SPEED
+			$PlayerAnimations.flip_h = false
+			facing = 1
 		
-	if Input.is_action_pressed("move_right"):
-		velocity.x = SPEED
-		$PlayerAnimations.flip_h = false
-		facing = 1
+		if Input.is_action_just_pressed("jump") and (is_on_floor() or jumpCount < 2):
+			if jumpCount == 0:
+				velocity.y = JUMP
+			if jumpCount == 1:
+				velocity.y = (JUMP * .70)
+			jumpCount += 1
 		
-	if Input.is_action_just_pressed("jump") and (is_on_floor() or jumpCount < 2):
-		if jumpCount == 0:
-			velocity.y = JUMP
-		if jumpCount == 1:
-			velocity.y = (JUMP * .70)
-		jumpCount += 1
-		
-	if Input.is_action_just_pressed("fire_projectile_light") and not firing and not hit:
-		firing = true
-		$PlayerAnimations.speed_scale = 2;
-		$PlayerAnimations.play("attack_g")
-		timer.start()
+		if Input.is_action_just_pressed("fire_projectile_light") and not firing and not hit:
+			firing = true
+			$PlayerAnimations.speed_scale = 2;
+			$PlayerAnimations.play("attack_g")
+			timer.start()
 
 	### handle some animatons stuff
 	if velocity.x > 5 or velocity.x < -5:
@@ -77,11 +80,12 @@ func _physics_process(_delta):
 	
 	### might play with this
 	velocity.y = GRAVITY + velocity.y
-		
+
 	velocity = move_and_slide(velocity, Vector2.UP)
-	
+
 	velocity.x = lerp(velocity.x, 0, .75)
-	
+
+
 func hide_show_lives():
 	var hud = self.get_parent().get_child(0)
 	if lives == 2:
@@ -90,17 +94,39 @@ func hide_show_lives():
 		hud.get_child(2).hide()
 	else:
 		hud.get_child(1).hide()
-		
-	
-	
+
+
+func bounce():
+	velocity.y = (JUMP * .65)
+
+
+func recoil(enemyX):
+	velocity.y = (JUMP * .45)
+	var myPos = global_transform.origin.x
+	if myPos < enemyX:
+		velocity.x = -900
+	elif myPos > enemyX:
+		velocity.x = 900
+
 func hurt(amount):
 	hit = true
+	can_move = false
 	$PlayerAnimations.play("hit")
+	set_collision_layer_bit(0, false)
+	set_collision_mask_bit(5, false)
 	hitTimer.start()
 	lives -= amount
 	hide_show_lives()
-	if lives == 0:
-		get_tree().change_scene("res://BaseLevel.tscn")
+	if lives <= 0:
+		set_collision_layer_bit(0, false)
+		set_collision_mask_bit(5, false)
+		reset()
+
+func reset():
+	$PlayerAnimations.play("death")
+	$DeathAnimation.start()
+	$DeathTimer.start()
+	
 
 func _on_Timer_timeout():
 	shoot_projectile()
@@ -120,4 +146,18 @@ func _on_FallZone_body_entered(_body):
 
 func _on_HitTimer_timeout():
 	hit = false
+	can_move = true
+	if lives > 0:
+		set_collision_layer_bit(0, true)
+		set_collision_mask_bit(5, true)
 	$PlayerAnimations.play("idle_g")
+
+
+func _on_DeathTimer_timeout():
+	var currentScene = get_tree().current_scene.name
+	get_tree().change_scene("res://" + currentScene +  ".tscn")
+
+
+func _on_DeathAnimation_timeout():
+	hide()
+	
